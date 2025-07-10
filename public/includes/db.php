31e62e19f2e7eb01de9b767413799c4664ce3340
Pedro Lara <pedro.c.lara.prestserv@petrobras.com.br>
2025-07-10 17:51:20 -0300

    Add DB includes file

---
+++ public/includes/db.php
@@ -0,0 +1,10 @@
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=eventos', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+} catch (PDOException $e) {
+    die('Erro na conexão: ' . $e->getMessage());
+}
